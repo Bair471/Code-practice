fc7f41345e99a6771dece24228780f0a3c353703
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2025-09-14 23:28:26 +0800

    Update practice.tsx

--- practice.tsx
+++ practice.tsx
@@ -15,3 +15,28 @@ function PersonRow({ person }) {
   return <p>{newPerson.name} ({newPerson.age})</p>;
 }
 
+
+function PersonRow({ person: { name, age } }) {
+  return <p>{name} ({age})</p>;
+}
+
+
+
+
+//person — это один пропс, который является объектом.
+
+//name, age, habits — это свойства объекта, к которым мы обращаемся через этот пропс.
+
+//const person = { id: 1, name: "Иван", age: 25, habits: ["спорт", "чтение"] };
+
+// Передача пропса:
+<PersonRow person={person} />
+
+// Внутри компонента:
+function PersonRow({ person }) {
+  console.log(person.name);  // "Иван"
+  console.log(person.age);   // 25
+}
+
+//передача по отдельности
+<PersonRow name={person.name} age={person.age} habits={person.habits} />
