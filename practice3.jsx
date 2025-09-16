1434fcf65b6650e8d391f7f6ba41aa53758f8179
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2025-09-16 23:17:12 +0800

    Update practice3.jsx

--- practice3.jsx
+++ practice3.jsx
@@ -9,3 +9,32 @@ const handleCloseModal = () => {
       .catch((err) => console.error('Ошибка', err));
     };
 
+// Выражение onSave={handleCloseModal} означает, что в компонент передаётся проп onSave, значением которого является функция handleCloseModal. 
+      <PersonModal
+        person={selectedPerson}
+        onSave={handleCloseModal}
+        onClose={handleCloseModal}
+      />
+    </>
+
+const handleSave = async () => {
+    try {
+      if (formData.id && formData.id !== 0) {
+        await fetch(`http://localhost:8000/persons/${formData.id}`, {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        });
+      } else {
+        await fetch("http://localhost:8000/persons", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        });
+      }
+      onSave();
+      onClose();
+    } catch (err) {
+      console.error("Ошибка сохранения:", err);
+    }
+  };
