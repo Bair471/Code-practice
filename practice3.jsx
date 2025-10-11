5959ada9d116e09d500dd844675c0232d34c6f8b
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2025-10-11 20:40:05 +0800

    Update practice3.jsx

--- practice3.jsx
+++ practice3.jsx
@@ -3,30 +3,41 @@ const TAX_RATE = 0.08;
 const PHONE_PRICE = 99.99;
 const ACCESSORY_PRICE = 9.99;
 
-let bank_balance = 500;
-let amount = 0;
+var bank_balance = 303.91;
+var amount = 0;
 
 function calculateTax(amount) {
-  return amount * TAX_RATE;
+	return amount * TAX_RATE;
 }
 
 function formatAmount(amount) {
-  return "$" + amount.toFixed(2);
+	return "$" + amount.toFixed( 2 );
 }
 
-while (amount + PHONE_PRICE <= bank_balance) {
-  amount += PHONE_PRICE;
-  if (amount < SPENDING_THRESHOLD) {
-    amount += ACCESSORY_PRICE;
-  }
+// продолжаем покупать телефоны пока у нас остаются деньги
+while (amount < bank_balance) {
+	// покупаем новый телефон!
+	amount = amount + PHONE_PRICE;
+
+	// можем ли мы позволить себе аксессуар?
+	if (amount < SPENDING_THRESHOLD) {
+		amount = amount + ACCESSORY_PRICE;
+	}
 }
 
-amount += calculateTax(amount);
+// не забудьте заплатить налог
+amount = amount + calculateTax( amount );
 
-console.log("Ваша покупка: " + formatAmount(amount));
+console.log(
+	"Ваша покупка: " + formatAmount( amount )
+);
+// Ваша покупка: $334.76
 
+// можете ли вы в самом деле позволить себе эту покупку?
 if (amount > bank_balance) {
-  console.log("Вы не можете позволить себе эту покупку. :(");
+	console.log(
+		"Вы не можете позволить себе эту покупку. :("
+	);
 }
 
 
