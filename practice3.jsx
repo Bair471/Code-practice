5b33b04f72a4c2827f7bc601f63ebbb859220c02
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2025-10-11 20:41:09 +0800

    Update practice3.jsx

--- practice3.jsx
+++ practice3.jsx
@@ -1,3 +1,23 @@
+function outer() {
+	var a = 1;
+
+	function inner() {
+		var b = 2;
+
+		// здесь у нас есть доступ и к `a`, и к `b`
+		console.log( a + b );	// 3
+	}
+
+	inner();
+
+	// здесь у нас есть доступ только к  `a`
+	console.log( a );			// 1
+}
+
+outer();
+
+
+
 const SPENDING_THRESHOLD = 200;
 const TAX_RATE = 0.08;
 const PHONE_PRICE = 99.99;
